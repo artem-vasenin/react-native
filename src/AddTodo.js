6d0a5b609b5b3dc702ca1f1fc22b80fef23b1ad9
Rusich <artem.skobar@yandex.ru>
2019-12-18 10:11:03 +0300

    validate add form

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+    View,
+    TextInput,
+    Button,
+    StyleSheet,
+    Alert
+} from 'react-native';
 
 export const AddTodo = ({ onSubmit }) => {
+    /** Создаем локальный стейт с начальным значением '' */
+    const [value, setValue] = useState('');
+
     const Submit = () => {
-        onSubmit('Test');
+        if (!value.trim()) {
+            /** выводим алертом ошибку */
+            Alert.alert('Заполните название задачи');
+        } else {
+            onSubmit(value);
+            setValue('');
+        }
     };
 
     return (
         <View style={styles.addTodo}>
-            <TextInput style={styles.input} />
+            <TextInput
+                style={styles.input}
+                /** передаем ф-цию стейта которая примет значнеие инпута */
+                onChangeText={setValue}
+                /** получаем значение value из стейта */
+                value={value}
+                placeholder='Введите название'
+            />
             <Button
                 style={styles.btn}
                 title="Добавить"
