f02928007f3ab5c844486ce9e69ab683ae2359b4
Rusich <artem.skobar@yandex.ru>
2019-12-19 15:58:12 +0300

    add styles for different platforms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import { THEME } from '../../theme';
 import { AppTextBold } from '../ui/AppTextBold';
 
 export const Navbar = props => {
     return (
-        <View style={styles.navbar}>
+        <View style={{
+            ...styles.navbar,
+            ...Platform.select({
+                ios: styles.navIos,
+                android: styles.navAndroid
+            })
+        }}>
             <AppTextBold style={styles.text}>
               {props.title}
             </AppTextBold>
@@ -18,11 +24,17 @@ const styles = StyleSheet.create({
         height: 70,
         alignItems: 'center',
         justifyContent: 'flex-end',
-        backgroundColor: THEME.COLOR_PRIMARY,
         paddingBottom: 10,
     },
+    navAndroid: {
+        backgroundColor: THEME.COLOR_PRIMARY,
+    },
+    navIos: {
+        borderBottomColor: THEME.COLOR_PRIMARY,
+        borderBottomWidth: 1,
+    },
     text: {
-        color: 'white',
+        color: Platform.OS === 'ios' ? 'black' : 'white',
         fontSize: 20,
     }
 });
